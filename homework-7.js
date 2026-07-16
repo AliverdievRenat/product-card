150e8fefc4c7f25b5cc2ae9e452ff82f08a8724c
AliverdievRenat <aliverdiev.renat05@gmail.com>
2026-07-16 22:59:57 +0700

    Addressed all the comments.

--- homework-7.js
+++ homework-7.js
@@ -1,38 +1,38 @@
-function showTemperature (city, temperature) {
-  return `Сейчас в ${city} температура : ${temperature} градусов по Цельсию`
-};
+function showTemperature(city, temperature) {
+  console.log(`Сейчас в ${city} температура : ${temperature} градусов по Цельсию`);
+}
 
-console.log(showTemperature("Санкт-Петербург", "+25"));
+showTemperature("Санкт-Петербург", "+25");
 
 
 const getSpeedCategory = speed => {
   const LIGHT_SPEED = 299792458;
   if (speed > LIGHT_SPEED) {
-    return "Сверх световая скорость"
+    console.log("Сверх световая скорость");
   }
   else if (speed < LIGHT_SPEED) {
-    return "Субсветовая скорость"
+    console.log("Субсветовая скорость");
   }
   else {
-    return "Скорость света"
+    console.log("Скорость света");
   }
-};
+}
 
 
-function purchaseProduct (budget, productName = "Lenovo ThinkPad", productPrice = 1999) {
+function purchaseProduct(budget, productName = "Lenovo ThinkPad", productPrice = 1999) {
   if (budget >= productPrice) {
-    return `${productName} приобретён. Спасибо за покупку!`
+    console.log(`${productName} приобретён. Спасибо за покупку!`);
   }
   else {
-    return `Вам не хватает ${productPrice - budget}$, пополните баланс`
+    console.log(`Вам не хватает ${productPrice - budget}$, пополните баланс`);
   }
-};
+}
 
 
 function isFavoriteBrand(brand) {
   const favoriteBrand = "Audi";
   return favoriteBrand === brand;
-};
+}
 
 
 const carBrand = "Audi";
