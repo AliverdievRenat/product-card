996ce44c4a4bdf39e9f8ad70e7a354a6de94722a
AliverdievRenat <aliverdiev.renat05@gmail.com>
2026-07-18 05:40:10 +0700

    Removed unnecessary indentation within and outside the functions.

--- homework-7.js
+++ homework-7.js
@@ -4,36 +4,29 @@ function showTemperature(city, temperature) {
 
 showTemperature("Санкт-Петербург", "+25");
 
-
 const getSpeedCategory = speed => {
   const LIGHT_SPEED = 299792458;
   if (speed > LIGHT_SPEED) {
     console.log("Сверх световая скорость");
-  }
-  else if (speed < LIGHT_SPEED) {
+  } else if (speed < LIGHT_SPEED) {
     console.log("Субсветовая скорость");
-  }
-  else {
+  } else {
     console.log("Скорость света");
   }
 }
 
-
 function purchaseProduct(budget, productName = "Lenovo ThinkPad", productPrice = 1999) {
   if (budget >= productPrice) {
     console.log(`${productName} приобретён. Спасибо за покупку!`);
-  }
-  else {
+  } else {
     console.log(`Вам не хватает ${productPrice - budget}$, пополните баланс`);
   }
 }
-
 
 function isFavoriteBrand(brand) {
   const favoriteBrand = "Audi";
   return favoriteBrand === brand;
 }
-
 
 const carBrand = "Audi";
 const carModel = "A6 Allroad";
